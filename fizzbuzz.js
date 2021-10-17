1096da95064bbb5db0a3bc7ac8215044257a87e8
“Chej <chejahamed@gmail.com>
2021-10-17 14:56:21 +0100

    Complete main functionality

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,22 @@
 function fizzbuzz(num){
-    if(num %3 === 0 && num %5 ===0){
+    if (num  === 0){
+        return num;
+    }
+
+    if(num % 3 === 0 && num % 5 ===0){
         return 'fizzbuzz'
     }
    
     if (num % 3 ===0){
         return 'fizz';
     }
-    if(num %5 ===0){
+    if(num % 5 ===0){
         return 'buzz'
     }
     return num;
 }
 function print(num){
-    for (let i=0;1<num;i++){
+    for (let i=0;i <=num;i++){
         console.log(`${i}:${fizzbuzz(i)}`)
     }
 }
