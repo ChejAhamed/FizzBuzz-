d7bcc49bd9d6d82658bd27b7176d36ce3b74dec9
“Chej <chejahamed@gmail.com>
2021-10-17 14:41:26 +0100

    Add test with fizzbuzz case

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,8 @@
 function fizzbuzz(num){
+    if(num %3 === 0 && num %5 ===0){
+        return 'fizzbuzz'
+    }
+   
     if (num % 3 ===0){
         return 'fizz';
     }
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -26,4 +26,10 @@ describe('fizzbuzz',()=>{
         const result=fizzbuzz(10)
         expect(expected).toBe(result)
     })
+    test('should print fizzbuzz if it receive a multiple of 3 and 5',()=>{
+        const expected='fizzbuzz';
+        const result=fizzbuzz(15)
+        expect(expected).toBe(result)
+    })
+
 })
